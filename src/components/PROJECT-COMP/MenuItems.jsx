633b9adb71db8ba69b15aff8dd33b86f155e92fb
Harshxgit <htp.codes@gmail.com>
2024-12-01 17:41:20 +0530

    add filter menuitem

--- src/components/PROJECT-COMP/MenuItems.jsx
+++ src/components/PROJECT-COMP/MenuItems.jsx
@@ -1,10 +1,37 @@
-import React from 'react'
+"use client";
+import React, { useEffect, useState } from "react";
+import { object } from "zod";
 
 export default function MenuItems() {
+  const [allmenu, setAllmenu] = useState([]);
+  const [getmenu, setGetmenu] = useState();
+  useEffect(async () => {
+    const menu = await getMenu();
+    setAllmenu(menu);
 
-  
+    const getmenu = allmenu.reduce((acc, curr) => {
+      if (!acc[curr.type]) {
+        acc.push(curr.type);
+      } else {
+        acc[curr.type] = curr;
+      }
+    }, {});
 
+    setGetmenu(getmenu);
+  });
   return (
-    <div>MenuItems</div>
-  )
+    <div>
+      <div>
+        {object.entries(getmenu).map(([itemType, item]) =>
+         (
+          <>
+          <div>{itemType}</div>
+          <div>{item.map((name)=>(
+             <>{name}</>
+          ))}</div>
+          </>
+        ))}
+      </div>
+    </div>
+  );
 }
