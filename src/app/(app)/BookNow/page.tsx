d7e23c8477d550a46d008715703aed45e0c24769
Harshxgit <htp.codes@gmail.com>
2024-12-01 20:17:59 +0530

    created order function on frontend

--- src/app/(app)/BookNow/page.tsx
+++ src/app/(app)/BookNow/page.tsx
@@ -1,7 +1,23 @@
-import React from 'react'
-
+import { order } from '@/app/actions/order/order'
+import React, { useState } from 'react'
+//get user details from session 
 export default function page() {
+  const [items , setItems] = useState({})
+
+  const onBook =()=>{
+    const sucess = order(items , userid , price ,partysize , totalmenuquantity )
+  }
+  const onDrag =({item}:any)=>{
+    if(item.name){
+      setItems(item.qty++)
+    }
+    setItems(item)
+  }
   return (
-    <div>hello harshu</div>
+    <div  >
+      <div>
+        Book Now
+      </div>
+    </div>
   )
 }
